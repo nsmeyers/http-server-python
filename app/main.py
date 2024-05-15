199166a876aee9c18d396fafd47cce874ff1d765
Nicholas Smeyers <nicholas.smeyers@outlook.com>
2024-05-15 12:29:46 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -10,8 +10,24 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
 
+    # request ok response
+    request_ok = b"HTTP/1.1 200 OK\r\n\r\n"
+
+    # request not found response
+    request_not_found = b"HTTP/1.1 404 Not Found\r\n\r\n"
+
     # send response
-    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    conn.sendall(request_ok)
+
+    # receive data
+    received_data = conn.recv(1024).decode("utf-8")
+    request_target = received_data.split()[1]
+
+    # check if request target is /test
+    if request_target == "/":
+        conn.sendall(request_ok)
+    else:
+        conn.sendall(request_not_found)
 
 
 if __name__ == "__main__":

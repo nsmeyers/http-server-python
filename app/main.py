1b2765dfa2a545c6dc971943ab92787548520bc2
Nicholas Smeyers <nicholas.smeyers@outlook.com>
2024-05-15 13:47:50 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -23,6 +23,7 @@ def main():
     # check if request target is /test
     if request_target == "/":
         conn.sendall(request_ok)
+
     elif request_target.startswith("/echo"):
         request_string = request_target[6:]
 
@@ -36,6 +37,24 @@ def main():
         #response
         response = f"{status_code}{content_type}{content_length}\r\n{request_string}"
         conn.sendall(response.encode("utf-8"))
+
+    elif request_target == "/user-agent":
+        # retrieve user agent
+        request_components = received_data.split("\r\n")
+        user_agent = request_components.where(lambda x: x.startswith("User-Agent: ")).first()
+        user_agent = user_agent.split(": ")[1]
+
+        # status code
+        status_code = "HTTP/1.1 200 OK\r\n"
+
+        # response headers
+        content_type = "Content-Type: text/plain\r\n"
+        content_length = f"Content-Length: {len(user_agent)}\r\n"
+
+        #response
+        response = f"{status_code}{content_type}{content_length}\r\n{user_agent}"
+        conn.sendall(response.encode("utf-8"))
+
     else:
         conn.sendall(request_not_found)
 
